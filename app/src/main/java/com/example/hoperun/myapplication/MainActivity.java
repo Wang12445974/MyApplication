8766d0ede7291a38f4c674d6844f6f9f229d4d8d
Wang chong <12445975@qq.com>
2017-08-09 14:23:57 +0800

    ssss

---
+++ app/src/main/java/com/example/hoperun/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hoperun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void send(){
+
+            String sss="";
+
+
+    }
+}
